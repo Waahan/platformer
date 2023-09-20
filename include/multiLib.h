beb3c8d7e4c866aa0a8ee9552cbafb68a2367027
Waahan <william@cryos.org>
2023-09-20 16:59:53 -0400

    Added audio music and sound classes
    music decodes the file on the fly while sound decodes it all at once
    audio is a interface for the functions that sound and music share
    
    Signed-off-by: Waahan <william@cryos.org>

--- include/multiLib.h
+++ include/multiLib.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <tuple>
 #include <vector>
+#include <chrono>
 
 extern "C"
 {
@@ -158,5 +159,95 @@ namespace multiLib
 
         Estd::custom_unique_ptr<TTF_Font, TTF_CloseFont> font;
         int fontSize = 36;
+    };
+
+    class audio
+    {
+        public:
+        audio() = default;
+
+        audio(const audio& copyFrom) = delete; 
+        audio& operator=(const audio& copyFrom) = delete; 
+
+        audio(audio&& moveFrom) = delete; 
+        audio& operator=(audio&& moveFrom) = delete; 
+
+        virtual ~audio() = default;
+
+        virtual bool paused() const =0;
+        virtual bool playing() const =0;
+
+        virtual audio& newVolume(int setVolume) =0;
+
+        virtual audio& play(int loops = 0) =0;
+
+        virtual audio& pause() =0;
+        virtual audio& resume() =0;
+        
+        virtual audio& stop() =0;
+    };
+
+    class music : public audio
+    {
+        public:
+        music(const std::string& path);
+
+        music(const music& copyFrom) = delete;
+        music& operator=(const music& copyFrom) = delete;
+
+        music(music&& moveFrom) = default;
+        music& operator=(music&& moveFrom) = default;
+
+        ~music() = default;
+
+        bool paused() const override;
+        bool playing() const override;
+        bool fading() const;
+
+        music& newVolume(int setVolume) override;
+
+        music& fadeIn(std::chrono::milliseconds fadeInFor, int loops = 0);
+        music& play(int loops = 0) override;
+
+        music& pause() override;
+        music& resume() override;
+
+        music& stop() override;
+        music& fadeOut(std::chrono::milliseconds fadeOutFor);
+
+        private:
+        Estd::custom_unique_ptr<Mix_Music, Mix_FreeMusic> currentMusic;
+    };
+
+    class sound : public audio
+    {
+        public:
+        sound(const std::string& path);
+        
+        sound(const sound& copyFrom) = delete;
+        sound& operator=(const sound& copyFrom) = delete;
+
+        sound(sound&& moveFrom) = default;
+        sound& operator=(sound&& moveFrom) = default;
+        
+        ~sound() = default;
+
+        bool paused() const override;
+        bool playing() const override;
+
+        sound& newVolume(int setVolume) override;
+
+        sound& play(int loops = 0) override;
+
+        sound& pause() override;
+        sound& resume() override;
+
+        sound& stop() override;
+
+        private:
+        bool ownsChannel() const;
+
+        Estd::custom_unique_ptr<Mix_Chunk, Mix_FreeChunk> currentChunk;
+        int channel;
     };
 } //multiLib
