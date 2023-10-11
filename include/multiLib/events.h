8de7710d0f38fd4e47ab06e2c41e31e07a2c3d0d
Waahan <william@cryos.org>
2023-10-10 21:37:21 -0400

    Added base for input system

--- include/multiLib/events.h
+++ include/multiLib/events.h
@@ -1,5 +1,51 @@
 #pragma once
 
+#include <vector>
+#include <functional>
+
+extern "C"
+{
+    #include <SDL2/SDL.h>
+}
+
 namespace multiLib
 {
-}
+    class eventHandler
+    {
+        public:
+        eventHandler() = default;
+
+        eventHandler(const eventHandler& copyFrom) = delete;
+        eventHandler& operator=(const eventHandler& copyFrom) = delete;
+
+        eventHandler(eventHandler&& moveFrom) = delete;
+        eventHandler& operator=(eventHandler&& moveFrom) = delete;
+
+        ~eventHandler() = default;
+
+        virtual bool useEvent(const SDL_Event& event) =0;
+    };
+
+    class eventMessager
+    {
+        public:
+        eventMessager();
+
+        eventMessager(const eventMessager& copyFrom) = delete;
+        eventMessager& operator=(const eventMessager& copyFrom) = delete;
+
+        eventMessager(eventMessager&& moveFrom) = default;
+        eventMessager& operator=(eventMessager&& moveFrom) = default;
+
+        ~eventMessager() = default;
+
+        void pollEvents();
+
+        void setExitCallback(std::function<void(void)>&& exitFunc);
+        eventMessager& addEventHandler(eventHandler* add);
+
+        private:
+        std::vector<eventHandler*> eventHandlers;
+        std::function<void(void)> exitFunction;
+    };
+} //multiLib
--- src/multiLib/events.cpp
+++ src/multiLib/events.cpp
@@ -1,1 +1,47 @@
 #include "multiLib/events.h"
+
+#include <cassert>
+
+namespace multiLib
+{
+    eventMessager::eventMessager()
+        : eventHandlers{}, exitFunction{[](){ exit(0); }}
+    {
+    }
+
+    void eventMessager::pollEvents()
+    {
+        SDL_Event event; 
+
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                exitFunction();
+            }
+
+            for(const auto& eventHandle : eventHandlers)
+            {
+                if(eventHandle->useEvent(event))
+                    break;
+            }
+        }
+    }
+
+    void eventMessager::setExitCallback(std::function<void(void)>&& exitFunc)
+    {
+        exitFunction = std::move(exitFunc);
+    }
+
+    eventMessager& eventMessager::addEventHandler(eventHandler* add)
+    {
+    /*
+        Precondition add is not a nullptr
+    */
+        assert(add && "eventMessager eventHandler can not be nullptr");
+
+        eventHandlers.push_back(add);
+
+        return *this;
+    }
+} //multiLib
